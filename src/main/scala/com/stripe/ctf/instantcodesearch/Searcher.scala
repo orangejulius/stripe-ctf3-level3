0a2ff741b26b57de67b24fa5c766e3960f68b7cc
Julian Simioni <julian.simioni@gmail.com>
2014-01-27 10:10:37 -0800

    Cache indexed code in memory

--- src/main/scala/com/stripe/ctf/instantcodesearch/Searcher.scala
+++ src/main/scala/com/stripe/ctf/instantcodesearch/Searcher.scala
@@ -5,6 +5,8 @@ import java.nio.file._
 
 import com.twitter.concurrent.Broker
 
+import scala.collection.mutable.HashMap
+
 abstract class SearchResult
 case class Match(path: String, line: Int) extends SearchResult
 case class Done() extends SearchResult
@@ -12,10 +14,11 @@ case class Done() extends SearchResult
 class Searcher(indexPath : String)  {
   val index : Index = readIndex(indexPath)
   val root = FileSystems.getDefault().getPath(index.path)
+  val cache = getFileCache(index.files)
 
   def search(needle : String, b : Broker[SearchResult]) = {
     for (path <- index.files) {
-      for (m <- tryPath(path, needle)) {
+      for (m <- tryCache(path, needle)) {
         b !! m
       }
     }
@@ -41,7 +44,26 @@ class Searcher(indexPath : String)  {
     return Nil
   }
 
+  def tryCache(path: String, needle: String) : Iterable[SearchResult] = {
+    val lines = cache(path)
+    lines.filter{ case(l,n) => l.contains(needle) }.
+    map { case(l,n) => new Match(path, n+1) }
+  }
+
   def readIndex(path: String) : Index = {
     new ObjectInputStream(new FileInputStream(new File(path))).readObject.asInstanceOf[Index]
+  }
+
+  def getFileLines(path: String) : Array[(String, Int)] = {
+    val text : String = slurp(root.resolve(path))
+    text.split("\n").zipWithIndex
+  }
+
+  def getFileCache(files: List[String]) : HashMap[String, Array[(String, Int)]] = {
+    val hash = new HashMap[String, Array[(String,Int)]]
+    files.foreach { file =>
+      hash += (file -> getFileLines(file))
+    }
+    hash
   }
 }
